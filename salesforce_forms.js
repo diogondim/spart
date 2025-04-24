2321659e5b70858d556f9b3e6de7adc8fcfc7817
Diogo Gondim <diogondim@hotmail.com>
2025-04-24 13:42:33 -0300

    Create salesforce_forms.js

---
+++ salesforce_forms.js
@@ -0,0 +1,33 @@
+function labeltoPlaceholder(sel) {
+
+    var inputs = document.querySelectorAll(sel);
+
+    for (var i = 0; i < inputs.length; i++) {
+        var input = inputs[i];
+        console.log(input);
+        var labelElement = input.closest('.smartcapture-controls').children[0].querySelector('label');
+        if(labelElement) {
+            var label = labelElement.textContent;
+            label = label.replace(' *', '');
+            input.setAttribute('placeholder', label);
+            labelElement.style.display = 'none';
+        }
+    
+    }
+
+};
+
+labeltoPlaceholder('input[type="text"]');
+labeltoPlaceholder('input[type="email"]');
+
+function removeNativeCss(){
+    var nativeCss = document.querySelector('link#smartcapture-stylesheet');
+    if(nativeCss) {
+        nativeCss.remove();
+    }
+};
+
+window.addEventListener("load", (event) => {
+    console.log('window loaded');
+    removeNativeCss();
+});
